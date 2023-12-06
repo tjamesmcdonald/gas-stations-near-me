1bb013ee085eded800305581cd203281df8aad50
Jescel James <jesceljames@gmail.com>
2023-12-06 10:51:15 -0500

    added starting structure: dependencies, data, functions, user interactions, initializations

---
+++ script.js
@@ -0,0 +1,26 @@
+
+// DEPENDENCIES (DOM Elements or Server Packages)
+
+
+
+// DATA (Global Variables or Imported Data)
+
+
+
+
+// FUNCTIONS (Helper Functions)
+
+fetch ()
+
+
+
+// USER INTERACTIONS
+
+
+
+
+
+
+// INITIALIZATION
+
+
